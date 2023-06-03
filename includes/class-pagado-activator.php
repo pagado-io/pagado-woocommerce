149ee18366f3fe44e5412f6aabeec3258feca92a
Sajidur Rahman <sajibsrs@gmail.com>
2023-06-03 14:02:59 +0600

    added plugin activator

---
+++ includes/class-pagado-activator.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link            #
+ * @since           0.1.0
+ *
+ * @package Pagado
+ * @subpackage Pagado/includes
+ */
+
+class PagadoActivator
+{
+    public static function activate()
+    {
+        // activation code
+    }
+}

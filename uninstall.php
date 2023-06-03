7be2113f24f8394637dcf6d15d33090e5bb4ec12
Sajidur Rahman <sajibsrs@gmail.com>
2023-06-03 14:00:42 +0600

    added uninstall file

---
+++ uninstall.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Fired when the plugin is uninstalled.
+ *
+ * @link            #
+ * @since           0.1.0
+ *
+ * @package         Pagado
+ */
+
+// If uninstall not called from WordPress, then exit.
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    exit;
+}

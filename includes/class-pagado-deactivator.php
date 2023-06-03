9b0f0b669483ff864105ea4ade42907986259676
Sajidur Rahman <sajibsrs@gmail.com>
2023-06-03 14:03:15 +0600

    added plugin deactivator

---
+++ includes/class-pagado-deactivator.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link            #
+ * @since           0.1.0
+ *
+ * @package Pagado
+ * @subpackage Pagado/includes
+ */
+
+class PagadoDeactivation
+{
+    public static function deactivate()
+    {
+        // deactivation code
+    }
+}

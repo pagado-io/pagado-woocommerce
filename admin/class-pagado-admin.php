55b8a85b7f8934c160ba926888434906d74171b4
Sajidur Rahman <sajibsrs@gmail.com>
2023-06-05 13:16:32 +0600

    init settings and menu page

--- admin/class-pagado-admin.php
+++ admin/class-pagado-admin.php
@@ -13,11 +13,13 @@ class PagadoAdmin
 {
     private $pluginName;
     private $version;
+    private $uiClass;
 
-    public function __construct($pluginName, $version)
+    public function __construct($uiClass, $pluginName, $version)
     {
         $this->pluginName = $pluginName;
         $this->version = $version;
+        $this->uiClass = $uiClass;
     }
 
     public function enqueueStyles()
@@ -35,9 +37,46 @@ class PagadoAdmin
     {
         wp_enqueue_script(
             $this->pluginName,
-            plugin_dir_url(__FILE__) . 'js/pagado-admin.js', array('jquery'),
+            plugin_dir_url(__FILE__) . 'js/pagado-admin.js',
+            array('jquery'),
             $this->version,
             false
+        );
+    }
+
+    public function settingsInit()
+    {
+        register_setting('pagado', 'pagado_options');
+
+        add_settings_section(
+            'pagado_section',
+            __('Section', 'pagado'),
+            array($this->uiClass, 'sectionHTML'),
+            'pagado'
+        );
+
+        add_settings_field(
+            'pagado_field',
+            __('Field', 'pagado'),
+            array($this->uiClass, 'fieldHTML'),
+            'pagado',
+            'pagado_section',
+            array(
+                'label_for' => 'pagado_field',
+                'class' => 'pagado_row',
+                'pagado_custom_data' => 'custom',
+            )
+        );
+    }
+
+    public function menuPageInit()
+    {
+        add_menu_page(
+            __('Pagado', 'pagado'),
+            'Pagado',
+            'manage_options',
+            'pagado',
+            array($this->uiClass, 'optionPageHTML'),
         );
     }
 }

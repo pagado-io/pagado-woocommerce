cade3d243a8037c8d7708b029aec3cdff0ee1941
Sajidur Rahman <sajibsrs@gmail.com>
2024-03-19 02:38:46 +0600

    added block support class

---
+++ includes/class-pagado-block-support.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Pagado payment gateway WooCommerce block support class.
+ *
+ * @link            #
+ * @since           2.2.0
+ *
+ * @package         Pagado
+ * @subpackage      Pagado/includes
+ */
+
+class Pagado_Block_Support
+{
+}

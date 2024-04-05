7d1f882b5fd1485bc731ca60d4ea620b9d7d9d16
Sajidur Rahman <sajibsrs@gmail.com>
2024-04-06 05:13:03 +0600

    restructured

--- pagado.php
+++ pagado.php
@@ -11,9 +11,11 @@
  * Plugin Name:         Pagado
  * Plugin URI:          https://pagado.io
  * Description:         Pagado payment processor for WooCommerce.
- * Version:             2.1.5
+ * Version:             2.1.6
  * Requires at least:   6.3
  * Requires PHP:        7.4
+ * Requires Plugins: woocommerce
+ * WC requires at least: 5.0.0
  * Author:              Pagado
  * Author URI:          https://pagado.io
  * License:             GPL-2.0+
@@ -22,38 +24,34 @@
  * Domain Path:         /languages
  */
 
-if (!defined('WPINC')) {
+if (!defined('ABSPATH')) {
     die;
 }
 
-if (is_plugin_active('woocommerce/woocommerce.php')) {
-    // Define constants
-    define('PAGADO_VERSION', '2.1.5');
-    define('PAGADO_ROOT', plugin_dir_path(__FILE__));
-    define('PAGADO_ROOT_URL', plugin_dir_url(__FILE__));
+// Define constants
+define('PAGADO_VERSION', '2.1.6');
+define('PAGADO_ROOT', plugin_dir_path(__FILE__));
+define('PAGADO_ROOT_URL', plugin_dir_url(__FILE__));
 
-    // Register activation and deactivation hooks
-    register_activation_hook(__FILE__, 'activate_pagado');
-    register_deactivation_hook(__FILE__, 'deactivate_pagado');
+// Register activation and deactivation hooks
+register_activation_hook(__FILE__, 'activate_pagado');
+register_deactivation_hook(__FILE__, 'deactivate_pagado');
 
-    // Initialize Pagado features
-    pagado_init();
-
-    // Activate Dokan support if it's active
-    if (is_plugin_active('dokan-lite/dokan.php') || is_plugin_active('dokan-pro/dokan.php')) {
-        require_once plugin_dir_path(__FILE__) . 'dokan/pagado-dokan-payment-method.php';
-    }
-} else {
-    // Display admin notice if WooCommerce is not activated
-    add_action('admin_notices', 'no_woocommerce_notice');
-}
+// Initialize Pagado features
+add_action('woocommerce_loaded', 'pagado_init');
+add_action('dokan_loaded', 'dokan_init');
 
 function pagado_init()
 {
     require_once plugin_dir_path(__FILE__) . 'includes/class-pagado.php';
     $pagado = new Pagado();
-    add_action('plugins_loaded', array($pagado, 'initialize'));
+    $pagado->initialize();
     // add_action('woocommerce_blocks_loaded', 'register_block_support');
+}
+
+function dokan_init()
+{
+    require_once plugin_dir_path(__FILE__) . 'dokan/pagado-dokan-payment-method.php';
 }
 
 function activate_pagado()
@@ -66,11 +64,6 @@ function deactivate_pagado()
 {
     require_once plugin_dir_path(__FILE__) . 'includes/class-pagado-activator.php';
     Pagado_Activator::deactivate();
-}
-
-function no_woocommerce_notice()
-{
-    echo '<div class="notice notice-error"><p><strong>WooCommerce is required for Pagado payment gateway. Please install and activate WooCommerce first.</strong></p></div>';
 }
 
 function register_block_support()

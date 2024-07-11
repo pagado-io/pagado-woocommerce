821a5bca70f53e0d1a85845833deb14cebae266a
Sajidur Rahman <sajibsrs@gmail.com>
2024-07-11 17:32:04 +0600

    version updated to 2.3.0

--- pagado.php
+++ pagado.php
@@ -29,7 +29,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Define constants
-define('PAGADO_VERSION', '2.2.1');
+define('PAGADO_VERSION', '2.3.0');
 define('PAGADO_ROOT', plugin_dir_path(__FILE__));
 define('PAGADO_ROOT_URL', plugin_dir_url(__FILE__));
 
